42eeb2b36f0bf4bd1095c813325af7366947b684
Jenzel Serapio <142371031+JenzelSerapio@users.noreply.github.com>
2024-03-22 15:26:14 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<html>
+   <head>
+        <title>CCC311-18/CCL311-18/CCC321-18/CCL321-18</title>
+		<style>
+		body {
+            background-image: url('background.jpg'); 
+            background-size: cover; 
+			background-repeat: no-repeat;
+            background-position: center; 
+            font-family: Arial, sans-serif; 
+        }
+        center {
+            text-align: center;
+        }
+        hr {
+             height: 5px; 
+            background: linear-gradient(to right, #64A7C4, #BAA8C1, #64A7C4); 
+            border: none; 
+            margin: 20px 0;
+			
+			}
+
+        center {
+            text-align: center;
+        }
+
+        h1, h3 {
+            font-family: 'Calibri'; 
+            color: #333; 
+        }
+		p {
+            font-family: Consolas; 
+            font-size: 20px;
+            color: #333; 
+        }
+		a {
+            font-family: 'Courier New', Courier, monospace; 
+			font-size: 25px;
+            text-decoration: none; 
+            color: #75524F; 
+			font-weight: bold;
+        }
+
+        a:hover {
+            color: #093A44; 
+        }
+       }
+    </style>
+   </head>
+   <body>
+   <center>
+   <hr/>
+   <h1>BootStrap</h1>
+   <h3>Section: 3 BSCS 1</h3>
+   <hr/>
+   <img src="studpic.jpeg" width="200" height="200"/>
+   <p><b>Name: JENZEL LEAH M. SERAPIO</b></p>
+   <h3><u>BOOTSTRAP ACTIVITIES</u></h3>
+   
+         <a href="Activity1">Activity1</a>
+		 <br/>
+		 <a href="Activity2">Activity2</a>
+		 <br/>
+		 <a href="Activity3">Activity3</a>
+         <br/>
+		 <a href="Activity4">Activity4</a>
+         <br/>
+		 <a href="Activity5">Activity5</a>
+         <br/>
+		 <a href="Activity6">Activity6</a>
+         <br/>
+		 <a href="Portfolio">Portfolio</a>
+		 <br/>
+		 <a href="photo_gallery">Photo Gallery</a>
+		 <br/>
+	     <a href="blog_project">Blog</a>
+		 <br/>
+	     <a href="e_commerce">E-Commerce</a>
+   </body>
+   </center>
+
+</html>
